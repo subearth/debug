abaf173be1b623d407d43a8fb1d1c60f4936bbbb
Saksham Kashyap <sakshamkashyap@hotmail.com>
2016-10-02 22:00:01 -0700

    Started Oxygen Tank pickup

---
+++ SubEarth/Source/SubEarth/Objects/OxygenTank.h
@@ -0,0 +1,23 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "Objects/Pickup.h"
+#include "OxygenTank.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SUBEARTH_API AOxygenTank : public APickup
+{
+	GENERATED_BODY()
+	
+public:
+
+	AOxygenTank();
+	void GetOxygenTank(void);
+	bool IsOxygenTankPickedUp = false;
+	
+	
+};
